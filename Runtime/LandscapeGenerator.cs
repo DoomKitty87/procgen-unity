cd9c8e481eadfae6ef725bf56f391f9101a5432a
DoomKitty87 <doomkitty887@gmail.com>
2023-05-26 12:25:06 -0700

    added lod for generation

--- Runtime/LandscapeGenerator.cs
+++ Runtime/LandscapeGenerator.cs
@@ -12,18 +12,24 @@ public class LandscapeGenerator : MonoBehaviour
   };
 
   [SerializeField] private noiseType noiseSelection = noiseType.perlin;
-
+  [Header("Generation Settings")]
   [SerializeField] private bool generateOnStart;
   [SerializeField] private bool generateOnUpdate;
   [SerializeField] private int tilesX;
   [SerializeField] private int tilesZ;
   [SerializeField] private int tileResolution;
   [SerializeField] private float tileScale;
+  [SerializeField] private float noiseScale;
   [SerializeField] private int waves;
   [SerializeField] private float amplitude;
   [SerializeField] private float frequency;
   [SerializeField] private int seed;
   [SerializeField] private float yOffset;
+
+  [Header("LOD Settings")]
+  [SerializeField] private bool useLOD;
+  [SerializeField] private Transform lodTarget;
+  [SerializeField] private float lodStartDistance;
 
   private void Start() {
     if (generateOnStart) GenerateLandscape();
@@ -49,8 +55,16 @@ public class LandscapeGenerator : MonoBehaviour
     }
     for (int x = 0; x < tilesX; x++) {
       for (int z = 0; z < tilesZ; z++) {
+        
         Vector3 position = new Vector3(x * tileResolution * tileScale, yOffset, z * tileResolution * tileScale);
-        GameObject tile = TileGeneration.GenerateTile(noise, position, seed, tileScale, tileResolution, tileResolution, tileScale, waves, amplitude, frequency);
+        GameObject tile;
+        if (useLOD) {
+          lodFac = (lodTarget.position - position).magnitude > lodStartDistance ? (((lodTarget.position - position).magnitude - lodStartDistance) > tileResolution ? tileResolution : (lodTarget.position - position).magnitude - lodStartDistance) : 1;
+          tile = TileGeneration.GenerateTile(noise, position, seed, tileScale * lodFac, tileResolution / lodFac, noiseScale, waves, amplitude, frequency);
+        }
+        else {
+          tile = TileGeneration.GenerateTile(noise, position, seed, tileScale, tileResolution, tileResolution, noiseScale, waves, amplitude, frequency);
+        }
         tile.transform.parent = transform;
       }
     }
